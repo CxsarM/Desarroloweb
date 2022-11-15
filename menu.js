e03440bae6016b2ba6c0ccdbd35709875876ff04
CxsarM <118215643+CxsarM@users.noreply.github.com>
2022-11-15 09:01:17 -0600

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,20 @@
+function mostrarclz() {
+    document.getElementById('menu__clz').style.display = 'block';
+    document.getElementById('menu__pzz').style.display = 'none';
+    document.getElementById('menu__pzne').style.display = 'none';
+
+}
+
+function mostrarpzz() {
+    document.getElementById('menu__pzz').style.display = 'block';
+    document.getElementById('menu__clz').style.display = 'none';
+    document.getElementById('menu__pzne').style.display = 'none';
+
+}
+
+function mostrarpzne() {
+    document.getElementById('menu__pzne').style.display = 'block';
+    document.getElementById('menu__clz').style.display = 'none';
+    document.getElementById('menu__pzz').style.display = 'none';
+
+}
